a4ada376da3e103602cf0a1b03936cd7a68197e0
abhi961 <abhi.cho824@gmail.com>
2024-08-11 14:50:13 +0530

    changes

--- DB/config.js
+++ DB/config.js
@@ -1,9 +1,25 @@
-const mongoose = require("mongoose");
-mongoose
-  .connect("mongodb://127.0.0.1:27017/snaptalk", {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    bufferCommands: false,
-  })
-  .then(() => console.log("Database connected!"))
-  .catch((err) => console.log(err));
+// const mongoose = require("mongoose");
+// mongoose
+//   .connect("mongodb://127.0.0.1:27017/snaptalk", {
+//     useNewUrlParser: true,
+//     useUnifiedTopology: true,
+//     bufferCommands: false,
+//   })
+//   .then(() => console.log("Database connected!"))
+//   .catch((err) => console.log(err));
+
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb://127.0.0.1:27017/snaptal', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  bufferCommands: false, // Disable buffering
+})
+.then(() => {
+  console.log('Database connected!');
+  // Start your application logic here
+})
+.catch(err => {
+  console.error('Database connection error:', err);
+  process.exit(1); // Exit the process if the connection fails
+});
